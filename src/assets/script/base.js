018f5c55ba3a1fafe8f043f81421b8c5638fc810
liuhao1234 <728672190@qq.com>
2019-03-15 15:58:25 +0800

    动画添加

--- src/assets/script/base.js
+++ src/assets/script/base.js
@@ -1,9 +1,75 @@
+
 $(function(){
+	$.initNavActive();
+	$.initIndexSlide();
+	$(".pro-list .list-cell").mouseenter(function(){
+		$(this).addClass("active").siblings(".list-cell").removeClass("active");
+	})
 	$('#fullpage').fullpage({
 		sectionsColor: ['#f87b7b', '#fff', '#86d19f', '#77afe6', '#fff', '#fff', '#7fc9c3'],
 		controlArrows: false,
+		afterLoad: function(anchorLink, index){
+			//导航动画
+			var $navLine = $(".nav-line");
+			$(".ws-nav-content ul li").eq(index-1).addClass("active").siblings("li").removeClass("active");
+			if(index === 1){
+				$(".ws-top-nav").removeClass("color-black");
+				window.currentWidth = 72;
+				window.commonWidth = 104;
+				$navLine.animate({
+					left:0,
+					width:window.currentWidth,
+				})
+			}else{
+				$(".ws-top-nav").addClass("color-black");
+				window.currentWidth = 48;
+				window.commonWidth = 76;
+				$navLine.animate({
+					left:window.currentWidth+window.commonWidth*(index-2),
+					width:window.commonWidth,
+				})
+			}
+
+
+
+			//首屏动画
+			if(index === 1){
+				$(".section01").addClass("active");
+			}else if(index === 2){
+				$(".section02").addClass("active");
+			}else if(index === 3){
+				$(".section03").addClass("active");
+			}else if(index === 4){
+				$(".section04").addClass("active");
+			}else if(index === 5){
+				$(".section05").addClass("active");
+			}else if(index === 6){
+				$(".section06").addClass("active");
+			}else if(index === 7){
+				$(".section07").addClass("active");
+			}
+
+		},
+		onLeave: function(index, nextIndex, direction){
+			
+		},
 	});
+
+
+	var mySwiper = new Swiper ('#mienList', {
+					    slidesPerView: 3,
+				      	spaceBetween: 30
+					})        
+
+	$(".us-tab li").click(function(){
+		$(this).addClass("active").siblings().removeClass("active");
+		var index = $(this).index()
+		$(".us-detail").find("li").eq(index).fadeIn().siblings().hide();
+	})
+	$(".jobs-tab").find("span").click(function(){
+		$(this).addClass("active").siblings().removeClass("active");
+	})
 	
 	//跳转到相应的页面
-	$.fn.fullpage.moveTo(7);
+	//$.fn.fullpage.moveTo(1);
 })
---
+++ src/assets/script/plugins.js
@@ -0,0 +1,54 @@
+$.extend({
+	initNavActive:function(){
+		var currentIndex = 1;
+		window.currentWidth = 72;
+		window.commonWidth = 104;
+		var overTimer = null;
+		var $navLine = $(".nav-line");
+		
+		//moveLine(index)
+		$(".ws-nav-content ul li").mouseenter(function(){
+			clearTimeout(overTimer);
+			var index = $(this).index();
+			moveLine(index)
+		})
+		$(".ws-nav-content ul li").mouseleave(function(){
+			overTimer = setTimeout(function(){
+				//var index = $(this).index();
+				moveLine(currentIndex)
+			},100)
+		})
+
+		$(".ws-nav-content ul li").click(function(){
+			clearTimeout(overTimer);
+			var $this = $(this);
+			var index = $this.index();
+			currentIndex = index;
+			window.currentWidth = index===1?72:48;
+			window.commonWidth = index===1?104:76;
+			moveLine(index);
+			$this.addClass("active").siblings("li").removeClass("active");
+
+			$.fn.fullpage.moveTo(index);
+		})
+
+		function moveLine(index){
+			var width = index===1?window.currentWidth:window.commonWidth;
+			var left = index===1?0:(window.currentWidth+window.commonWidth*(index-2));
+
+			//$this.addClass("active").siblings("li").removeClass("active");
+			$navLine.animate({
+				left:left,
+				width:width,
+			})
+		}
+	},
+
+	initIndexSlide:function(){
+		$(".slide-guide li").click(function(){
+			var index = $(this).index();
+			$(this).addClass("active").siblings("li").removeClass("active");
+			$.fn.fullpage.moveTo(1,index);
+		})
+	}
+})
